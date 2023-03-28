62ffe44b3d47593ddd2ae9672b4f21c3221123f3
Christian Fenn <cfenn2020@fau.edu>
2023-03-28 14:52:24 -0400

    Completed Navbar template

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+import Navbar from "@/scenes/navbar";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("");
 
   return (
-    <div className="app">
-      app
+    <div className="app bg-gray-20">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
